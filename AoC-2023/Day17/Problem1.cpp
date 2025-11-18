daefdb01e1021046a06097879def04278ed446aa
Kuldeep9071 <kuldeepjatav@iisc.ac.in>
2025-11-19 05:10:57 +0530

    Day17 Solutions

---
+++ AoC-2023/Day17/Problem1.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int M, N;
+#define vi vector<int>
+const vector<vector<int>> dirs = {{1,0}, {0,1}, {-1, 0}, {0,-1}};
+
+bool in(int x, int y){
+    return x>=0 && y>=0 && x < M && y < N;
+}
+
+void solve() {
+    string line;
+    vector<string> grid;
+    while(getline(cin, line)) grid.push_back(line);
+
+    M = grid.size(), N = grid[0].size();
+
+    set<vi> seen;
+    priority_queue<vi, vector<vi>, greater<vi>> pq;
+
+    // heatLoss, x, y, dx, dy, steps left in single direction
+    // Don't have to include initial cell heatLoss
+    if(in(1,0)) pq.push({0, 1,0,1,0, 2});
+    if(in(0,1)) pq.push({0, 0,1,0,1, 2});
+    
+    while(not pq.empty()){
+        const vi curr = pq.top();
+        pq.pop();
+
+        const int heatLoss = curr[0], x = curr[1], y = curr[2];
+        const int dx = curr[3], dy = curr[4], stepsLeft = curr[5];
+
+        if(x == M-1 && y == N-1){
+            cout << heatLoss + (grid[x][y] - '0') << "\n";
+            break;
+        }
+
+        const vi state = {x,y,dx,dy,stepsLeft};
+        if(seen.find(state) != seen.end()) continue;
+        seen.insert(state);
+
+        for(const vi &dir: dirs){
+            int da = dir[0], db = dir[1];
+
+            if((da == dx && db == dy) || (da == -dx && db == -dy)) continue;
+
+            if(in(x+da, y+db)){
+                pq.push({heatLoss + (grid[x][y] - '0'), x+da, y+db, da, db, 2});
+            }
+        }
+
+        if(stepsLeft > 0 && in(x+dx, y+dy)){
+            pq.push({heatLoss + (grid[x][y] - '0'), x+dx, y+dy, dx, dy, stepsLeft-1});
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+}
