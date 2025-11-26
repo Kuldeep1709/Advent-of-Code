77dbd99f471af2ab264c559c9516c4ebfd29e05b
Kuldeep9071 <kuldeepjatav@iisc.ac.in>
2025-11-26 13:05:26 +0530

    Day13 Solutions

---
+++ AoC-2015/Day13/Problem2.py
@@ -0,0 +1,45 @@
+import re
+family = dict()
+pattern = r"(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)."
+
+with open("Day13/input.txt") as f:
+    for line in f:
+        match = re.match(pattern, line)
+
+        if match:
+            user1, action, number, user2 = match.groups()
+
+            happiness = int(number)
+
+            if action == "lose":
+                happiness = -happiness
+            
+            if user1 not in family:
+                family[user1] = {}
+
+            family[user1][user2] = happiness
+            
+
+visited = set()
+
+def optimal(curr, start, mini):
+    if len(visited) == len(family)-1: 
+        return family[curr][start] + family[start][curr] - mini
+
+    ans = 0
+    visited.add(curr)
+
+    for next in family[curr]:
+        if next not in visited:
+            ans = max(ans, optimal(next, start,min(mini, family[curr][next] + family[next][curr])) + family[curr][next] + family[next][curr])
+
+    visited.remove(curr)
+
+    return ans
+
+start = ""
+for member in family:
+    start = member
+    break
+
+print(optimal(start, start, float('inf')))
